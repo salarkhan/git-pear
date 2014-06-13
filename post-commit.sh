75e5a1ec6c02189da746def124c176e277e8d15f
salarkhan <salar.k3@gmail.com>
2014-06-13 01:20:19 -0700

    create authors file && cat authors to it * set git user name & email * pseudocode eventual functionality

--- post-commit.sh
+++ post-commit.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# add authors
+# usage: auth add salarkhan salar.k3@gmail.com
+# result: cat this pair into .git/authors
+function create_authors_file {
+    test -e ./authors.txt || touch ./authors.txt
+}
+
+function add_author {
+    echo $1 $2 >> ./authors.txt
+}
+
+# set authors
+function set_user {
+    git config --local user.name $1
+}
+
+function set_email {
+    git config --local user.email $1
+}
+
+# store default user name & email
+# list authors // return authors array
+# remove author // remove from hash lookup
+# reset authors to default // empty authors array
+
+# get user email from their username
 function ping_github {
     user_data=$(curl -i https://api.github.com/users/$1)
 }
